e1ce4ff921448bc13373e3c1d85ce327b027db6a
rob-canning-coventry <rob.canning@coventry.ac.uk>
2016-05-20 09:04:10 +0100

    twitter bot mods and example handinsheet mods

--- dm_twitter_bot.py
+++ dm_twitter_bot.py
@@ -1,8 +1,7 @@
-
-
 import tweepy
 import random
 import time
+import string
 
 class TwitterAPI:
     def __init__(self):
@@ -17,29 +16,33 @@ class TwitterAPI:
     def tweet(self, message):
         self.api.update_status(status=message)
 
+####################################
 careers = [
-"Digital Marketing Consultant",
-"Junior Front End Designer",
-"UX Consultant",
-"Interactive Media Designer",
-"Mobile Application Developer",
-"Augmented Reality Designer",
-"Agile Scrum Master #projectmanagement",
-"Devops Guru"
+    "Digital Marketing Consultant",
+    "Junior Front End Designer",
+    "UX Consultant",
+    "Interactive Media Designer",
+    "Mobile Application Developer",
+    "Augmented Reality Designer",
+    "Agile Scrum Master #projectmanagement",
+    "Devops Guru"
 ]
 
-careertweet = "Thinking about #careerdestinations? how about a " + random.choice(careers) + " http://blablacareerjob.com"
+careertweet = "Thinking about #careerdestinations? how about a " + \
+                random.choice(careers) + " http://blablacareerjob.com"
 
 ###################################
 advantagemodules = [
-"Digital and Social Networks",
-"",
-""
+    "Digital and Social Networks",
+    "",
+    ""
 ]
+
 optionmodules = [
-"Picturing the Body",
-"Experimental Narratives: Module leader: Dom Breadmore"
+    "Picturing the Body",
+    "Experimental Narratives: Module leader: Dom Breadmore"
 ]
+
 optionstweet = "2ndYr, which option module will you choose? " + random.choice(optionmodules) + "?"
 advantagetweet = "advantage modules, a great way to broaden your skills! which will you choose? " + random.choice(advantagemodules)
 ###################################
@@ -48,52 +51,50 @@ coremodulesYr2 = ["Transmedia 204MC", "International Creative Hacklab 204MC", "D
 coremodulesYr3 = ["Final Project 333MC", "Creative Hacklab III 304MC", " 204MC"]
 ###################################
 generaltweets = [
-"I'm the digital media twitter bot! #awesome #code I tweet a couple of times a day..",
-"http://coventry.media/digitalmedia #awsome #digitalmedia #degree #uk",
-"Studying #digitalmedia in #coventryuni - get some l33t 5k1llz #beyonddigitalliteracy",
-"Current running module = Creative Hacklab #104MC",
-"#robisawesome",
-"#digitalmediacov Lecturers: Adnan Hadzi (Senior Lecturer), Rob Canning (Lecturer in Connected Digital Media)",
-"the best #digitalmediacov workshops with #digitalmediacov Associate Lectuer Dan",
-"#worldleading #skillssessions with Navneet Bains, Paul Adkins and Becks Stewart #digitalmediacov",
-"hello world!",
-"#freestuff don't forget to sign up for your https://education.github.com/pack to get your $100 #digitalocean credits"
-"learning #git workflows, loving the https://education.github.com/pack",
-"https://www.blender.org #blender #3D",
-"Vector graphics fun with #inkscape https://inkscape.org/en/",
-"#markdown (.md files) a great way to sketch out webpages and take lecture notes!",
-"write in markdown then convert to html docx pdf etc with #pandoc #awesomepower",
-"#audacity a great tool for making #podcasts",
-"#Gimp, perfect for things with pixels, no... not vectors, #rasters!",
-"make awsome montages with #imagemagick #commandline fun",
-"want #projectmanagement skills? get clued in to Agile using http://tagia.io",
-"Want to learn #AdobeCreativeSuite? - don't forget Becks and Nav run a session every #wedensday during term time in ETG05."
+    "I'm the digital media twitter bot! #awesome #code I tweet a couple of times a day..",
+    "http://coventry.media/digitalmedia #awsome #digitalmedia #degree #uk",
+    "Studying #digitalmedia in #coventryuni - get some l33t 5k1llz #beyonddigitalliteracy",
+    "Current running module = Creative Hacklab #104MC",
+    "#robisawesome",
+    "#digitalmediacov Lecturers: Adnan Hadzi (Senior Lecturer), Rob Canning (Lecturer in Connected Digital Media)",
+    "the best #digitalmediacov workshops with #digitalmediacov Associate Lectuer Dan",
+    "#worldleading #skillssessions with Navneet Bains, Paul Adkins and Becks Stewart #digitalmediacov",
+    "hello world!",
+    "#freestuff don't forget to sign up for your https://education.github.com/pack to get your $100 #digitalocean credits"
+    "learning #git workflows, loving the https://education.github.com/pack",
+    "https://www.blender.org #blender #3D",
+    "Vector graphics fun with #inkscape https://inkscape.org/en/",
+    "#markdown (.md files) a great way to sketch out webpages and take lecture notes!",
+    "write in markdown then convert to html docx pdf etc with #pandoc #awesomepower",
+    "#audacity a great tool for making #podcasts",
+    "#Gimp, perfect for things with pixels, no... not vectors, #rasters!",
+    "make awsome montages with #imagemagick #commandline fun",
+    "want #projectmanagement skills? get clued in to Agile using http://tagia.io",
+    "Want to learn #AdobeCreativeSuite? - don't forget Becks and Nav run a session every #wedensday during term time in ETG05."
 ]
 ###################################
 readinglist = [
-"The Network Society by Manuel Castells",
-"The New Media Reader by ",
-""
+    "The Network Society by Manuel Castells",
+    "The New Media Reader by ",
+    "Bla Bla"
 ]
 readingtweet = "Wondering what to read next? Try " + random.choice(readinglist)
 ###################################
 watchinglist = [
-"Internets Own Boy: The story of Aaron Swartz",
-"CitizenFour #edwardsnowden",
-"The Social Network: The story of Facebook"
+    "Internets Own Boy: The story of Aaron Swartz",
+    "CitizenFour #edwardsnowden",
+    "The Social Network: The story of Facebook"
 ]
 watchingtweet = "Looking for something to watch tonight? try " + random.choice(watchinglist) + " #itsallresearch"
 ###################################
-
-commandoftheday = [
-"",
-""
-
+commands = [
+    "convert -resize 1920x foo.png bar.jpg",
+    "qrencode -size 30 http://someurl.org -o qrcode_someurl.png"
 ]
+commandoftheday = "#commandoftheday" + random.choice(commands) + "#digitalmediacov"
+###################################
 
-
-
-allothertweets = [readingtweet, watchingtweet, careertweet, optionstweet, advantagetweet];
+allothertweets = [readingtweet, watchingtweet, careertweet, optionstweet, advantagetweet, commandoftheday];
 
 if __name__ == "__main__":
     twitter = TwitterAPI()
--- example-handin-sheet.md
+++ example-handin-sheet.md
@@ -1,3 +1,5 @@
+[Digital Copy Here: https://github.com/rob-canning-coventry/creative-hacklab-104MC/blob/master/example-handin-sheet.md](https://github.com/rob-canning-coventry/creative-hacklab-104MC/blob/master/example-handin-sheet.md)
+
 # Example hand-in sheet for 104MC Creative Hacklab (To submit to Turnitin)
 
 ## Project Name and Members
@@ -9,13 +11,15 @@
 ## Group Github Repo
 
 Link to *private* group github/bitbucket repository
-- https://github.com/rob-canning-coventry/creative-hacklab-104MCf
+
+- https://github.com/rob-canning-coventry/creative-hacklab-104MC
 
 > Please use your README.md to give any licence information / extra notes / explanation / of your project.
 
 ## Media Assets too large for Github / not appropriate for version control.
 
 Link to dropbox/googledrive/owncloud storage for any media assets too large for github
+
 - https://www.dropbox.com/sh/osdfup40qij/AACge148Psd6rrjSa?dl=0
 
 ## Your Private Reflective Journal Repo
@@ -23,7 +27,9 @@ Link to your personal *private* reflective journal repository
 
 - https://github.com/rob-canning-coventry/creative-hacklab-104MC
 
-*Make sure rob-canning-coventry is added as a contributor to your repositories!*
+> Please use your README.md to give additional information  on your personal contribution to the project.
+
+_Make sure rob-canning-coventry is added as a contributor to your repositories!_
 
 ## My contribition to the Group out of 10
 
